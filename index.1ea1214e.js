aae798a1d705b6bf68b8e90949a2764380e18455
LirikTop <melnicukkiril2002@gmail.com>
2024-11-15 22:11:56 +0200

    make build

---
+++ index.1ea1214e.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("table").rows,l=!0,r=!1,t=void 0;try{for(var n,o=e[Symbol.iterator]();!(l=(n=o.next()).done);l=!0){var a=n.value;if(a.cells.length>1){var c=a.cells[1].cloneNode(!0);a.cells[a.cells.length-1].insertAdjacentElement("beforebegin",c)}}}catch(e){r=!0,t=e}finally{try{l||null==o.return||o.return()}finally{if(r)throw t}}
+//# sourceMappingURL=index.1ea1214e.js.map
